5395b71a6d853340ddff2165cdccaed4f6f77659
DaveDevPerry <daveperry.log@gmail.com>
2021-06-05 20:30:45 +0100

    progress bar working and styled

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
 
+const curTime = document.querySelector('#current-time');
+const songTime = document.querySelector('#song-time');
+const progressBar = document.querySelector('#progress');
+
 // Song titles
 const songs = [
 	'04 Discotheque Wreck',
@@ -93,6 +97,115 @@ function nextSong() {
 
 	playSong();
 }
+
+//get song duration
+function getSongDuration(e) {
+	console.log(e.srcElement.duration);
+	const durInSecs = Math.round(e.srcElement.duration);
+	let secs = (durInSecs % 60).toString();
+	secs < 10 ? (secs = secs.padStart(2, '0')) : secs;
+	console.log(secs);
+	let mins = Math.floor(durInSecs / 60).toString();
+	mins < 10 ? (mins = mins.padStart(2, '0')) : mins;
+	console.log(mins);
+	songTime.innerHTML = `${mins}:${secs}`;
+
+	// mins || secs === NaN
+	// 	? (songTime.innerHTML = '')
+	// 	: (songTime.innerHTML = `${mins}:${secs}`);
+	// mins || secs != NaN
+	// 	? (songTime.innerHTML = `${mins}:${secs}`)
+	// 	: (songTime.innerHTML = '');
+}
+
+//get current time
+function getCurrentTime(e) {
+	const durInSecs = Math.round(e.srcElement.currentTime);
+	// console.log(currentTime);
+	let secs = (durInSecs % 60).toString();
+	secs < 10 ? (secs = secs.padStart(2, '0')) : secs;
+	let mins = Math.floor(durInSecs / 60).toString();
+	mins < 10 ? (mins = mins.padStart(2, '0')) : mins;
+	curTime.innerHTML = `${mins}:${secs}`;
+}
+
+// update progress bar
+function updateProgressBar(e) {
+	const { currentTime, duration } = e.srcElement;
+	let percentPlayed = Math.round((currentTime / duration) * 100);
+	console.log(percentPlayed);
+	// let test = (currentTime / duration) * 100;
+	// let testA = test.toFixed(1);
+	// console.log(test, testA);
+	// progressBar.style.width = `${testA}px`;
+	progressBar.style.width = `${percentPlayed}%`;
+}
+
+//get duration & currentTime for Time of song
+function DurTime(e) {
+	console.log(e);
+	const { duration, currentTime } = e.srcElement;
+	console.log(duration, currentTime);
+	let sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+	min = min < 10 ? '0' + min : min;
+
+	// define seconds currentTime
+	function get_sec(x) {
+		if (Math.floor(x) >= 60) {
+			for (var i = 1; i <= 60; i++) {
+				if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+					sec = Math.floor(x) - 60 * i;
+					sec = sec < 10 ? '0' + sec : sec;
+				}
+			}
+		} else {
+			sec = Math.floor(x);
+			sec = sec < 10 ? '0' + sec : sec;
+		}
+		console.log(x);
+	}
+
+	get_sec(currentTime, sec);
+
+	// change currentTime DOM
+	curTime.innerHML = min + ':' + sec;
+
+	// define minutes duration
+	let min_d = isNaN(duration) === true ? '0' : Math.floor(duration / 60);
+	min_d = min_d < 10 ? '0' + min_d : min_d;
+	console.log(min_d);
+
+	function get_sec_d(x) {
+		if (Math.floor(x) >= 60) {
+			for (var i = 1; i <= 60; i++) {
+				if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+					sec_d = Math.floor(x) - 60 * i;
+					sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+				}
+			}
+		} else {
+			sec_d = isNaN(duration) === true ? '0' : Math.floor(x);
+			sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
+		}
+	}
+
+	// define seconds duration
+
+	get_sec_d(duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d + ':' + sec_d;
+}
+
+// Time of song
+// audio.addEventListener('timeupdate', DurTime);
+audio.addEventListener('timeupdate', getSongDuration);
+audio.addEventListener('timeupdate', getCurrentTime);
+audio.addEventListener('timeupdate', updateProgressBar);
 
 // Update progress bar
 // function updateProgress(e) {
