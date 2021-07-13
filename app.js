3d4106fe8411b04b9f653a472c9ac40fda34fa21
DaveDevPerry <daveperry.log@gmail.com>
2021-07-14 00:10:49 +0100

    added pause when returning to home page, added hover color change on circle btns

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ backBtn.addEventListener('click', () => {
 	// show landing page
 	document.querySelector('.now-playing').classList.remove('section-active');
 	document.querySelector('.landing').classList.add('section-active');
-	// pauseSong();
+	pauseSong();
 });
 // Event listeners
 playBtn.addEventListener('click', playMusic);
