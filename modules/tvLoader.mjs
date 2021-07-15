a83b4eb4f1adb729b518bd353e8e7c7d9e63d477
DaveDevPerry <daveperry.log@gmail.com>
2021-07-15 22:18:48 +0100

    loader speed shortened and glitch effect added to terror-fi on landing page

--- modules/tvLoader.mjs
+++ modules/tvLoader.mjs
@@ -4,7 +4,7 @@ export function tvLoader(){
     document.querySelector('.tv-loader')
     // .style.display = 'none';
     const tl = new TimelineMax();
-    tl.to('.tv-loader',1,{y:'100%'})
+    tl.to('.tv-loader',0.5,{y:'100%'})
 
     // make a gsap transition to slide the loader downwards
   },5000)
